b642c10c915aeaf68951b859abe2ab3f3723e03b
Monika Rathor <monika.soni@gmail.com>
2019-01-29 11:52:31 -0800

    Convert ID to int

--- server/server.js
+++ server/server.js
@@ -13,310 +13,310 @@ const  { resolvers } = require('./schema.js');*/
 
 const db = {
 	products: [    {
-        id: "1",
+        id: 1,
         name: "Sony ICF-SC1",
         model: "icf-sc1",
-        brand: "110",
-        category: "1004",
+        brand: 110,
+        category: 1004,
         userguide: "https:\/\/user.guide\/index.php?from=6"
     },
     {
-        id: "2",
+        id: 2,
         name: "Sony SFM1",
         model: "sfm1",
-        brand: "110",
-        category: "1004",
+        brand: 110,
+        category: 1004,
         userguide: "https:\/\/user.guide\/index.php?from=7"
     },
     {
-        id: "3",
+        id: 3,
         name: "Canon Imageformula Scanfront 300p Cac\/Piv Network Scanner 4575B007",
         model: "canon_imageformula_scanfront_300p_cac_piv_network_scanner_4575b007",
-        brand: "103",
-        category: "1004",
+        brand: 103,
+        category: 1004,
         userguide: "https:\/\/user.guide\/index.php?from=261298"
     },
     {
-        id: "4",
+        id: 4,
         name: "Canon Scanner 101",
         model: "canon_scanner_101",
-        brand: "103",
-        category: "1004",
+        brand: 103,
+        category: 1004,
         userguide: "https:\/\/user.guide\/index.php?from=261299"
     },
     {
-        id: "5",
+        id: 5,
         name: "Sony DPP-M55",
         model: "dpp-m55",
-        brand: "110",
-        category: "1003",
+        brand: 110,
+        category: 1003,
         userguide: "https:\/\/user.guide\/index.php?from=234934"
     },
     {
-        id: "6",
+        id: 6,
         name: "Sony LPR-1000MD",
         model: "lpr-1000md",
-        brand: "110",
-        category: "1003",
+        brand: 110,
+        category: 1003,
         userguide: "https:\/\/user.guide\/index.php?from=234935"
     },
     {
-        id: "7",
+        id: 7,
         name: "Apple LQ",
         model: "lq",
-        brand: "102",
-        category: "1003",
+        brand: 102,
+        category: 1003,
         userguide: "https:\/\/user.guide\/index.php?from=235046"
     },
     {
-        id: "8",
+        id: 8,
         name: "Apple B2867757",
         model: "b2867757",
-        brand: "102",
-        category: "1003",
+        brand: 102,
+        category: 1003,
         userguide: "https:\/\/user.guide\/index.php?from=235047"
     },
     {
-        id: "9",
+        id: 9,
         name: "Apple 6500",
         model: "6500",
-        brand: "102",
-        category: "1003",
+        brand: 102,
+        category: 1003,
         userguide: "https:\/\/user.guide\/index.php?from=235048"
     },
     {
-        id: "10",
+        id: 10,
         name: "Epson COLOR 850",
         model: "color_850",
-        brand: "105",
-        category: "1003",
+        brand: 105,
+        category: 1003,
         userguide: "https:\/\/user.guide\/index.php?from=236174"
     },
     {
-        id: "11",
+        id: 11,
         name: "Epson COLOR 860",
         model: "color_860",
-        brand: "105",
-        category: "1003",
+        brand: 105,
+        category: 1003,
         userguide: "https:\/\/user.guide\/index.php?from=236175"
     },
     {
-        id: "12",
+        id: 12,
         name: "Sony Mobile Xperia tipo ST21A",
         model: "sony_mobile_xperia_tipo_st21a",
-        brand: "110",
-        category: "1001",
+        brand: 110,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=48354"
     },
     {
-        id: "13",
+        id: 13,
         name: "Philips CTS900",
         model: "cts900",
-        brand: "108",
-        category: "1001",
+        brand: 108,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=48507"
     },
     {
-        id: "14",
+        id: 14,
         name: "Philips D-44867",
         model: "d-44867",
-        brand: "108",
-        category: "1001",
+        brand: 108,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=48508"
     },
     {
-        id: "15",
+        id: 15,
         name: "Samsung CURVE 9330",
         model: "curve_9330",
-        brand: "109",
-        category: "1001",
+        brand: 109,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=48639"
     },
     {
-        id: "16",
+        id: 16,
         name: "Samsung DFX-5000",
         model: "dfx-5000",
-        brand: "109",
-        category: "1001",
+        brand: 109,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=48640"
     },
     {
-        id: "17",
+        id: 17,
         name: "Samsung DM-S105",
         model: "dm-s105",
-        brand: "109",
-        category: "1001",
+        brand: 109,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=48641"
     },
     {
-        id: "18",
+        id: 18,
         name: "Apple iPhone 4S A1431",
         model: "iphone_4s_a1431",
-        brand: "102",
-        category: "1001",
+        brand: 102,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=49342"
     },
     {
-        id: "19",
+        id: 19,
         name: "Apple iPhone 4s MD257LL\/A",
         model: "iphone_4s_md257ll_a",
-        brand: "102",
-        category: "1001",
+        brand: 102,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=49343"
     },
     {
-        id: "20",
+        id: 20,
         name: "Apple iPhone 4 A1332",
         model: "iphone_4_a1332",
-        brand: "102",
-        category: "1001",
+        brand: 102,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=49344"
     },
     {
-        id: "21",
+        id: 21,
         name: "Apple iPhone 4 MC536LL\/A",
         model: "iphone_4_mc536ll_a",
-        brand: "102",
-        category: "1001",
+        brand: 102,
+        category: 1001,
         userguide: "https:\/\/user.guide\/index.php?from=49345"
     },
     {
-        id: "22",
+        id: 22,
         name: "Sony CG-FX120K",
         model: "cg-fx120k",
-        brand: "110",
-        category: "1002",
+        brand: 110,
+        category: 1002,
         userguide: "https:\/\/user.guide\/index.php?from=158711"
     },
     {
-        id: "23",
+        id: 23,
         name: "Acer TravelMate 800 Series",
         model: "travelmate_800_series",
-        brand: "101",
-        category: "1002",
+        brand: 101,
+        category: 1002,
         userguide: "https:\/\/user.guide\/index.php?from=159672"
     },
     {
-        id: "24",
+        id: 24,
         name: "Dell Laptop 7537",
         model: "dell_laptop_7537",
-        brand: "104",
-        category: "1002",
+        brand: 104,
+        category: 1002,
         userguide: "https:\/\/user.guide\/index.php?from=160773"
     },
     {
-        id: "25",
+        id: 25,
         name: "IBM THINKPAD X32",
         model: "thinkpad_x32",
-        brand: "106",
-        category: "1002",
+        brand: 106,
+        category: 1002,
         userguide: "https:\/\/user.guide\/index.php?from=162774"
     },
     {
-        id: "26",
+        id: 26,
         name: "IBM THINKVANTAGE T60",
         model: "thinkvantage_t60",
-        brand: "106",
-        category: "1002",
+        brand: 106,
+        category: 1002,
         userguide: "https:\/\/user.guide\/index.php?from=162775"
     },
     {
-        id: "27",
+        id: 27,
         name: "Lenovo Flex 15 15.6 59387570",
         model: "lenovo_flex_15_15_6_59387570",
-        brand: "107",
-        category: "1002",
+        brand: 107,
+        category: 1002,
         userguide: "https:\/\/user.guide\/index.php?from=163176"
     },
     {
-        id: "28",
+        id: 28,
         name: "Lenovo G500 59373044",
         model: "lenovo_g500_59373044",
-        brand: "107",
-        category: "1002",
+        brand: 107,
+        category: 1002,
         userguide: "https:\/\/user.guide\/index.php?from=163177"
     },
     {
-        id: "29",
+        id: 29,
         name: "Lenovo THINKPAD W510",
         model: "thinkpad_w510",
-        brand: "107",
-        category: "1002",
+        brand: 107,
+        category: 1002,
         userguide: "https:\/\/user.guide\/index.php?from=165738"
     }
 	],
   categories: [
 		{
 			name:"cell_phone",
-			products:["12", "13", "14", "15", "16", "17", "18", "19", "20", "21"],
-			id:"1001"
+			products:[12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+			id: 1001
 		},
 		{
 			name:"laptop",
-			products:["24", "25", "26", "27", "28", "29"],
-			id:"1002"
+			products:[24, 25, 26, 27, 28, 29],
+			id: 1002
 		},
 		{
 			name:"printer",
-			products:["5", "6", "7", "8", "9", "10", "11"],
-			id:"1003"
+			products:[5, 6, 7, 8, 9, 10, 11],
+			id: 1003
 		},
 		{
 			name:"scanner",
-			products:["1", "2", "3", "4"],
-			id:"1004"
+			products:[1, 2, 3, 4],
+			id: 1004
 		}
 	],
   brands: [
 		{
 			name:"acer",
-			products:["23"],
-			id:"101"
+			products:[23],
+			id: 101
 		},
 		{
 			name:"apple",
-			products:["7", "8", "9", "18", "19", "20", "21"],
-			id:"102"
+			products:[7, 8, 9, 18, 19, 20, 21],
+			id: 102
 		},
 		{
 			name:"canon",
-			products:["3", "4"],
-			id:"103"
+			products:[3, 4],
+			id: 103
 		},
 		{
 			name:"dell",
-			products:["24"],
-			id:"104"
+			products:[24],
+			id: 104
 		},
 		{
 			name:"epson",
-			products:["10", "11"],
-			id:"105"
+			products:[10, 11],
+			id: 105
 		},
 		{
 			name:"ibm",
-			products:["25", "26"],
-			id:"106"
+			products:[25, 26],
+			id: 106
 		},
 		{
 			name:"lenovo",
-			products:["27", "28", "29"],
-			id:"107"
+			products:[27, 28, 29],
+			id: 107
 		},
 		{
 			name:"philips",
-			products:["13", "14"],
-			id:"108"
+			products:[13, 14],
+			id: 108
 		},
 		{
 			name:"samsung",
-			products:["15", "16", "17"],
-			id:"109"
+			products:[15, 16, 17],
+			id: 109
 		},
 		{
 			name:"sony",
-			products:["1", "2", "5", "6","12", "22"],
-			id:"110"
+			products:[1, 2, 5, 6,12, 22],
+			id: 110
 		}
 	]
 };
@@ -324,17 +324,17 @@ const db = {
 const typeDefs = gql`
 	type Query {
     products: [Product]
-    product(id: ID!): Product
+    product(id: Int!): Product
     productsOfBrand(brand: String): [Product]
     productsOfCategory(category: String): [Product]
     productsOfBrandAndCategory(category : String, brand: String): [Product]
     brands: [Brand]
-    brand(id: ID!): Brand
+    brand(id: Int!): Brand
     categories: [Category]
-    category(id: ID!): Category
+    category(id: Int!): Category
   }
   type Product {
-    id: ID
+    id: Int
     brand: Brand
     category: Category
     model: String
@@ -343,12 +343,12 @@ const typeDefs = gql`
   }
   type Brand {
     products: [Product]
-    id: ID
+    id: Int
     name: String
   }
   type Category {
     products: [Product]
-    id: ID
+    id: Int
     name: String
   }
 `;
